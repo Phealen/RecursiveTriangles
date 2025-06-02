42108cf0f4cc81b3c973a7587dfa88cb1f703fad
Phealen <phelanjanzen@gmail.com>
2025-06-02 12:45:37 +0200

    init commit

---
+++ src/triangle/Triangle.java
@@ -0,0 +1,64 @@
+package triangle;
+
+import resizable.ResizableImage;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import static resizable.Debug.print;
+
+/**
+ * Implement your Sierpinski Triangle here.
+ *
+ *
+ * You only need to change the drawTriangle
+ * method!
+ *
+ *
+ * If you want to, you can also adapt the
+ * getResizeImage() method to draw a fast
+ * preview.
+ *
+ */
+public class Triangle implements ResizableImage {
+    int drawTriangle = 0;
+    /**
+     * change this method to implement the triangle!
+     * @param size the outer bounds of the triangle
+     * @return an Image containing the Triangle
+     */
+    private BufferedImage drawTriangle(Dimension size) {
+        print("drawTriangle: " + ++drawTriangle + "size: " + size);
+        BufferedImage bufferedImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D gBuffer = (Graphics2D) bufferedImage.getGraphics();
+        gBuffer.setColor(Color.black);
+        int border = 2;
+        gBuffer.drawRect(border, border, size.width - 2 * border, size.height - 2 * border);
+        gBuffer.setColor(Color.darkGray);
+        border = 8;
+        gBuffer.drawRect(border, border, size.width - 2 * border, size.height - 2 * border);
+        gBuffer.drawString("Triangle goes here", border * 2, border * 4);
+        return bufferedImage;
+    }
+
+    BufferedImage bufferedImage;
+    Dimension bufferedImageSize;
+
+    @Override
+    public Image getImage(Dimension triangleSize) {
+        if (triangleSize.equals(bufferedImageSize))
+            return bufferedImage;
+        bufferedImage = drawTriangle(triangleSize);
+        bufferedImageSize = triangleSize;
+        return bufferedImage;
+    }
+    @Override
+    public Image getResizeImage(Dimension size) {
+        BufferedImage bufferedImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D gBuffer = (Graphics2D) bufferedImage.getGraphics();
+        gBuffer.setColor(Color.pink);
+        int border = 2;
+        gBuffer.drawRect(border, border, size.width - 2 * border, size.height - 2 * border);
+        return bufferedImage;
+    }
+}
